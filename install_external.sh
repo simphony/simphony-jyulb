ef4f5ee7055c8adb0457fa0a558e71b0a548d145
Ioannis Tziakos <ioannist@enthought.com>
2015-03-11 21:02:33 +0000

    Update install_external.sh

--- install_external.sh
+++ install_external.sh
@@ -3,5 +3,4 @@
 git clone https://github.com/simphony/JYU-LB.git
 cd JYU-LB
 make
-cd ..
-ln -s /usr/local/bin/jyu_lb_isothermal3D.exe ./JYU-LB/bin/jyu_lb_isothermal3D.exe
+ln -s /usr/local/bin/jyu_lb_isothermal3D.exe bin/jyu_lb_isothermal3D.exe
