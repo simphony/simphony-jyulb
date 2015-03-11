47322e7b806b75f7d88b6eaec6f66d404d9f22b0
Ioannis Tziakos <ioannist@enthought.com>
2015-03-11 20:49:24 +0000

    create a simlink to /usr/local/bin

--- install_external.sh
+++ install_external.sh
@@ -4,4 +4,4 @@ git clone https://github.com/simphony/JYU-LB.git
 cd JYU-LB
 make
 cd ..
-mv JYU-LB/bin/jyu_lb_isothermal3D.exe .
+ln -s /usr/local/bin/jyu_lb_isothermal3D.exe /JYU-LB/bin/jyu_lb_isothermal3D.exe
