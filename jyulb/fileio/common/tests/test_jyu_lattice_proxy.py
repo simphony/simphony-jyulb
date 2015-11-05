25b6aed3e1db927588b849d2b23da879b4b63b51
tuopuu <tuomas.a.puurtinen@jyu.fi>
2015-11-05 16:48:14 +0200

    Moved overridden tests into a common template for internal and fileio based wrappers. Some pep8 work.

--- jyulb/fileio/common/tests/test_jyu_lattice_proxy.py
+++ jyulb/fileio/common/tests/test_jyu_lattice_proxy.py
@@ -3,14 +3,12 @@ import unittest
 import numpy as np
 
 from simphony.core.cuba import CUBA
-from simphony.core.keywords import KEYWORDS
 from simphony.core.data_container import DataContainer
-from simphony.cuds.lattice import LatticeNode
-from simphony.testing.utils import (create_data_container)
 from jyulb.fileio.common.jyu_lattice_proxy import JYULatticeProxy
 from simphony.testing.abc_check_lattice import (
-    CheckLatticeContainer, CheckLatticeNodeOperations,
-    CheckLatticeNodeCoordinates)
+    CheckLatticeContainer, CheckLatticeNodeCoordinates)
+from jyulb.testing.jyu_check_proxy_lattice import (
+    JYUProxyLatticeNodeOperations)
 
 
 def _create_zeroed_lattice(name, primitive_cell, size, origin):
@@ -25,37 +23,6 @@ def _create_zeroed_lattice(name, primitive_cell, size, origin):
     return JYULatticeProxy(name, primitive_cell, size, origin, ext_ndata)
 
 
-def _create_data_with_zero_values(restricted_cuba):
-    """ Return a DataContainer containing ZERO values
-
-    Parameters
-    ----------
-    restricted_cuba : CUBA
-        The cuba keys which should appear
-
-    """
-    data = {cuba: _zero_value(cuba) for cuba in restricted_cuba}
-    return DataContainer(data)
-
-
-def _zero_value(cuba):
-    keyword = KEYWORDS[CUBA(cuba).name]
-    if np.issubdtype(keyword.dtype, str):
-        return keyword.name
-    else:
-        shape = keyword.shape
-        if shape == [1]:
-            if np.issubdtype(keyword.dtype, 'float'):
-                return float(0)
-            if np.issubdtype(keyword.dtype, 'int'):
-                return int(0)
-        else:
-            if np.issubdtype(keyword.dtype, 'float'):
-                return np.zeros(shape=shape, dtype=np.float64)
-            if np.issubdtype(keyword.dtype, 'int'):
-                return np.zeros(shape=shape, dtype=np.int32)
-
-
 class TestJYULatticeProxyContainer(CheckLatticeContainer, unittest.TestCase):
 
     """Test case for JYULatticeProxy class."""
@@ -66,7 +33,7 @@ class TestJYULatticeProxyContainer(CheckLatticeContainer, unittest.TestCase):
         return set(CUBA)
 
 
-class TestJYULatticeProxyNodeOperations(CheckLatticeNodeOperations,
+class TestJYULatticeProxyNodeOperations(JYUProxyLatticeNodeOperations,
                                         unittest.TestCase):
 
     def container_factory(self, name, primitive_cell, size, origin):
@@ -75,70 +42,13 @@ class TestJYULatticeProxyNodeOperations(CheckLatticeNodeOperations,
     def supported_cuba(self):
         return [CUBA.MATERIAL_ID, CUBA.DENSITY, CUBA.VELOCITY, CUBA.FORCE]
 
-    def test_get_node(self):
-        # TODO https://github.com/simphony/simphony-common/issues/216
-        # For some tests in CheckLatticeNodeOperations it is expected
-        # that the `container_factory` has produced a lattice with nodes
-        # that are empty.  And that then these nodes can be updated on
-        # the lattice with the 'supported_cuba'. JYULatticeProxy requires
-        # that the supported cuba appear in the `external_node_data` parameter
-        # (what CUBA appear on each node.data cannot be changed).  We
-        # therefore override some tests to ensure that we are testing
-        # the getters and iters using the node-data values provided in
-        # the `container_factory`.
-        container = self.container
+    def _create_data_with_zero_values(self):
+        """ Return a DataContainer containing ZERO values for the
+        supported CUBA keys
 
-        index = 2, 3, 4
-        node = container.get_node(index)
-
-        expected = LatticeNode(index, data=_create_data_with_zero_values(
-            self.supported_cuba()))
-        self.assertEqual(node, expected)
-
-        # check that mutating the node does not change internal info
-        node.data = create_data_container()
-        self.assertNotEqual(container.get_node(index), node)
-
-    def test_iter_nodes(self):
-        # TODO Overriding this method (see test_get_node above).
-        # https://github.com/simphony/simphony-common/issues/216
-        container = self.container
-
-        # number of nodes
-        number_of_nodes = sum(1 for node in container.iter_nodes())
-        self.assertEqual(number_of_nodes, np.prod(self.size))
-
-        # data
-        for node in container.iter_nodes():
-            self.assertEqual(node.data, _create_data_with_zero_values(
-                self.supported_cuba()))
-
-        # indexes
-        x, y, z = np.meshgrid(
-            range(self.size[0]), range(self.size[1]), range(self.size[2]))
-        expected = set(zip(x.flat, y.flat, z.flat))
-        indexes = {node.index for node in container.iter_nodes()}
-        self.assertEqual(indexes, expected)
-
-    def test_iter_nodes_subset(self):
-        # TODO Overriding this method (see test_get_node above).
-        # https://github.com/simphony/simphony-common/issues/216
-        container = self.container
-
-        x, y, z = np.meshgrid(
-            range(2, self.size[0]),
-            range(self.size[1]-4),
-            range(3, self.size[2], 2))
-        expected = set(zip(x.flat, y.flat, z.flat))
-
-        # data
-        for node in container.iter_nodes(expected):
-            self.assertEqual(node.data, _create_data_with_zero_values(
-                self.supported_cuba()))
-
-        # indexes
-        indexes = {node.index for node in container.iter_nodes(expected)}
-        self.assertEqual(indexes, expected)
+        """
+        return DataContainer(MATERIAL_ID=0, DENSITY=0.0,
+                             VELOCITY=[0, 0, 0], FORCE=[0, 0, 0])
 
 
 class TestJYULatticeProxyNodeCoordinates(CheckLatticeNodeCoordinates,
--- jyulb/internal/common/tests/test_proxy_lattice.py
+++ jyulb/internal/common/tests/test_proxy_lattice.py
@@ -3,17 +3,15 @@ import unittest
 import numpy as np
 
 from simphony.core.cuba import CUBA
-from simphony.core.keywords import KEYWORDS
 from simphony.cuds.lattice import LatticeNode
-from simphony.cuds.abc_lattice import ABCLattice
 from simphony.core.data_container import DataContainer
 from jyulb.internal.common.proxy_lattice import ProxyLattice
 from jyulb.internal.common.domain import PyLattice, PyGeometry
 from jyulb.internal.common.domain import PyIsothermalData
-from simphony.testing.utils import (create_data_container)
 from simphony.testing.abc_check_lattice import (
-    CheckLatticeContainer, CheckLatticeNodeOperations,
-    CheckLatticeNodeCoordinates)
+    CheckLatticeContainer, CheckLatticeNodeCoordinates)
+from jyulb.testing.jyu_check_proxy_lattice import (
+    JYUProxyLatticeNodeOperations)
 
 
 def _create_zeroed_lattice(name, primitive_cell, size, origin):
@@ -37,37 +35,6 @@ def _create_zeroed_lattice(name, primitive_cell, size, origin):
 
     return ProxyLattice(name, primitive_cell, geom, fdata)
 
-def _create_data_with_zero_values(restricted_cuba):
-    """ Return a DataContainer containing ZERO values
-
-    Parameters
-    ----------
-    restricted_cuba : CUBA
-        The cuba keys which should appear
-
-    """
-    data = {cuba: _zero_value(cuba) for cuba in restricted_cuba}
-    data[CUBA.MATERIAL_ID] = ProxyLattice.FLUID_ENUM
-    return DataContainer(data)
-
-
-def _zero_value(cuba):
-    keyword = KEYWORDS[CUBA(cuba).name]
-    if np.issubdtype(keyword.dtype, str):
-        return keyword.name
-    else:
-        shape = keyword.shape
-        if shape == [1]:
-            if np.issubdtype(keyword.dtype, 'float'):
-                return float(0)
-            if np.issubdtype(keyword.dtype, 'int'):
-                return int(0)
-        else:
-            if np.issubdtype(keyword.dtype, 'float'):
-                return np.zeros(shape=shape, dtype=np.float64)
-            if np.issubdtype(keyword.dtype, 'int'):
-                return np.zeros(shape=shape, dtype=np.int32)
-
 
 class TestProxyLatticeContainer(CheckLatticeContainer, unittest.TestCase):
 
@@ -79,7 +46,7 @@ class TestProxyLatticeContainer(CheckLatticeContainer, unittest.TestCase):
         return set(CUBA)
 
 
-class TestProxyLatticeNodeOperations(CheckLatticeNodeOperations,
+class TestProxyLatticeNodeOperations(JYUProxyLatticeNodeOperations,
                                      unittest.TestCase):
 
     def container_factory(self, name, primitive_cell, size, origin):
@@ -88,83 +55,27 @@ class TestProxyLatticeNodeOperations(CheckLatticeNodeOperations,
     def supported_cuba(self):
         return [CUBA.MATERIAL_ID, CUBA.DENSITY, CUBA.VELOCITY, CUBA.FORCE]
 
+    def _create_data_with_zero_values(self):
+        """ Return a DataContainer containing ZERO values for the
+        supported CUBA keys. CUBA.MATERIAL_ID is set to correspond to
+        fluid lattice node value.
 
-    def test_get_node(self):
-        # TODO https://github.com/simphony/simphony-common/issues/216
-        # For some tests in CheckLatticeNodeOperations it is expected
-        # that the `container_factory` has produced a lattice with nodes
-        # that are empty.  And that then these nodes can be updated on
-        # the lattice with the 'supported_cuba'. JYULatticeProxy requires
-        # that the supported cuba appear in the `external_node_data` parameter
-        # (what CUBA appear on each node.data cannot be changed).  We
-        # therefore override some tests to ensure that we are testing
-        # the getters and iters using the node-data values provided in
-        # the `container_factory`.
-        container = self.container
-
-        index = 2, 3, 4
-        node = container.get_node(index)
-
-        expected = LatticeNode(index, data=_create_data_with_zero_values(
-            self.supported_cuba()))
-        self.assertEqual(node, expected)
-
-        # check that mutating the node does not change internal info
-        node.data = create_data_container()
-        self.assertNotEqual(container.get_node(index), node)
-
-    def test_iter_nodes(self):
-        # TODO Overriding this method (see test_get_node above).
-        # https://github.com/simphony/simphony-common/issues/216
-        container = self.container
-
-        # number of nodes
-        number_of_nodes = sum(1 for node in container.iter_nodes())
-        self.assertEqual(number_of_nodes, np.prod(self.size))
-
-        # data
-        for node in container.iter_nodes():
-            self.assertEqual(node.data, _create_data_with_zero_values(
-                self.supported_cuba()))
-
-        # indexes
-        x, y, z = np.meshgrid(
-            range(self.size[0]), range(self.size[1]), range(self.size[2]))
-        expected = set(zip(x.flat, y.flat, z.flat))
-        indexes = {node.index for node in container.iter_nodes()}
-        self.assertEqual(indexes, expected)
-
-    def test_iter_nodes_subset(self):
-        # TODO Overriding this method (see test_get_node above).
-        # https://github.com/simphony/simphony-common/issues/216
-        container = self.container
-
-        x, y, z = np.meshgrid(
-            range(2, self.size[0]),
-            range(self.size[1]-4),
-            range(3, self.size[2], 2))
-        expected = set(zip(x.flat, y.flat, z.flat))
-
-        # data
-        for node in container.iter_nodes(expected):
-            self.assertEqual(node.data, _create_data_with_zero_values(
-                self.supported_cuba()))
-
-        # indexes
-        indexes = {node.index for node in container.iter_nodes(expected)}
-        self.assertEqual(indexes, expected)
-
+        """
+        return DataContainer(MATERIAL_ID=ProxyLattice.FLUID_ENUM,
+                             DENSITY=0.0, FORCE=[0, 0, 0],
+                             VELOCITY=[0, 0, 0])
 
     def test_update_nodes(self):
-        """ ProxyLattice only supports updating lattice nodes with
-        CUBA.MATERIAL_ID defined as ProxyLattice.FluidEnum
+        """ This test is overridden because ProxyLattice only supports
+        updating lattice nodes with CUBA.MATERIAL_ID defined as
+        ProxyLattice.FluidEnum
         """
         container = self.container
 
         indices = ((2, 3, 4), (1, 2, 3))
         nodes = [container.get_node(index) for index in indices]
         for node in nodes:
-            node.data = _create_data_with_zero_values(self.supported_cuba())
+            node.data = self._create_data_with_zero_values()
         container.update_nodes(nodes)
 
         for n in xrange(len(indices)):
@@ -175,16 +86,18 @@ class TestProxyLatticeNodeOperations(CheckLatticeNodeOperations,
             self.assertIsNot(new_node, nodes[n])
 
     def test_update_nodes_with_extra_keywords(self):
-        """ ProxyLattice only supports updating lattice nodes with
-        CUBA.MATERIAL_ID defined as ProxyLattice.FluidEnum
+        """ This test is overridden because ProxyLattice only supports
+        updating lattice nodes with CUBA.MATERIAL_ID defined as
+        ProxyLattice.FluidEnum
         """
         container = self.container
 
         indices = ((2, 3, 4), (1, 2, 3))
         nodes = [container.get_node(index) for index in indices]
-        # Update with full DataContainer.
+        # Update with DataContainer with non supported keys.
         for node in nodes:
-            node.data = _create_data_with_zero_values(set(CUBA))
+            node.data = self._create_data_with_zero_values()
+            node.data = DataContainer(node.data, RADIUS=1)
         container.update_nodes(nodes)
 
         for n in xrange(len(indices)):
@@ -193,11 +106,20 @@ class TestProxyLatticeNodeOperations(CheckLatticeNodeOperations,
             # We expect only the supported CUBA to be stored.
             expected = LatticeNode(
                 index=nodes[n].index,
-                data=_create_data_with_zero_values(self.supported_cuba()))
+                data=self._create_data_with_zero_values())
             self.assertEqual(new_node, expected)
             # Check that `new_node` is not the same instance as `node`
             self.assertIsNot(new_node, nodes[n])
 
+
+class TestProxyLatticeNodeCoordinates(CheckLatticeNodeCoordinates,
+                                      unittest.TestCase):
+
+    def container_factory(self, name, primitive_cell, size, origin):
+        return _create_zeroed_lattice(name, primitive_cell, size, origin)
+
+    def supported_cuba(self):
+        return [CUBA.MATERIAL_ID, CUBA.DENSITY, CUBA.VELOCITY, CUBA.FORCE]
 
 
 if __name__ == '__main__':
